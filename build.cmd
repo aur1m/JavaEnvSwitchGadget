4e65724f60439a1ec44f8d62bc3d7bb69dfa8ec1
ICE <ice.sumy@gmail.com>
2014-08-02 00:10:15 +0300

    [+] ready-to-use build [~] build shell
    Signed-off-by: ICE <ice.sumy@gmail.com>

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 del JavaEnvSwitchGadget.gadget
-"C:\Program Files\7-Zip\7z.exe" a -tzip -x!.idea -x!build.cmd -r JavaEnvSwitchGadget *.*
+"C:\Program Files\7-Zip\7z.exe" a -tzip -x!.idea -x!.git -x!_stuff -x!build.cmd -r JavaEnvSwitchGadget *.*
 move JavaEnvSwitchGadget.zip JavaEnvSwitchGadget.gadget
 pause
