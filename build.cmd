7700881a26e7bcd10b00a60b550d75c9c80988b2
ICE <ice.sumy@gmail.com>
2014-08-02 00:05:10 +0300

    Initial commit
    Signed-off-by: ICE <ice.sumy@gmail.com>

---
+++ build.cmd
@@ -0,0 +1,4 @@
+del JavaEnvSwitchGadget.gadget
+"C:\Program Files\7-Zip\7z.exe" a -tzip -x!.idea -x!build.cmd -r JavaEnvSwitchGadget *.*
+move JavaEnvSwitchGadget.zip JavaEnvSwitchGadget.gadget
+pause
